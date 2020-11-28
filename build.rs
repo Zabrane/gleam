13b10942d0a8e799e1973fb1277c810064e5bab9
Louis Pilfold <louis@lpil.uk>
2020-11-28 20:26:59 +0000

    Cap'n Proto based data serialization

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
     lalrpop::process_root().unwrap();
+
+    // TODO: comment this out
+    capnpc::CompilerCommand::new()
+        .file("src/bindata.capnp")
+        .output_path("./")
+        .run()
+        .expect("compiling src/bindata.capnp");
 }
